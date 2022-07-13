1c65714cbc4484ed1b693bc9c146661e188b8ebb
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-07-13 16:09:59 +0100

    Add all code for merkile-tree use-case

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
